a72b2fec0c9090700669277bfe24eacd5fc53960
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-21 20:24:10 +0530

    Employee wage calculation use switch case statement

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -10,22 +10,26 @@ public class EmployeeWage {
 
     static final int ABSENT = 0;
 
-    public static void employee(){
+    public static void employee() {
         int empWage = 0;
 
-        int randomNumber = (int) Math.floor(Math.random()*10)%3;
-        if (randomNumber ==FULL_DAY){
-            System.out.println("Employee is full day Present");
-            empWage = FULL_DAY_HOUR*WAGE_PER_HOUR;
+        int randomNumber = (int) Math.floor(Math.random() * 10) % 3;
+        switch (randomNumber) {
+            case (FULL_DAY):
+                System.out.println("Employee is full day Present");
+                empWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                break;
 
-        } else if (randomNumber == HALF_DAY) {
-            System.out.println("Employee is half day Present");
-            empWage = HALF_DAY_HOUR*WAGE_PER_HOUR;
+            case (HALF_DAY):
+                System.out.println("Employee is half day Present");
+                empWage = HALF_DAY_HOUR * WAGE_PER_HOUR;
+                break;
+            default:
+                System.out.println("Employee is Absent");
 
-        } else {
-            System.out.println("Employee is Absent");
+
         }
-        System.out.println("Employee wage is : "+empWage);
+        System.out.println("Employee wage is : " + empWage);
     }
 
     public static void main(String[] args) {
