c5b00e7bf2c51987c57f362f7ed03972ad7903e2
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-02 14:39:59 +0530

    Ability to save total wage for each company

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,5 +1,5 @@
 package com.bridgelabz;
-import java.util.Scanner;
+
 /**
  * @author GovardhanReddy
  */
@@ -10,63 +10,75 @@ public class EmployeeWage {
      */
     public static final int FULL_DAY = 1;
     public static final int HALF_DAY = 2;
-    public static final int FULL_DAY_HOUR=8;
-    public static final int HALF_DAY_HOUR=4;
-    public static void employee(String companyName ,int wagePerHour ,int monthlyWorkingDays ,int monthlyWorkingHours ) {
+    public static final int FULL_DAY_HOUR = 8;
+    public static final int HALF_DAY_HOUR = 4;
+    String companyName;
+    int wagePerHour;
+    int monthlyWorkingDays;
+    int monthlyWorkingHours;
+
+    /**
+     * use parameterized constructor
+     *
+     * @param companyName
+     * @param wagePerHour
+     * @param monthlyWorkingDays
+     * @param monthlyWorkingHours
+     */
+    public EmployeeWage(String companyName, int wagePerHour, int monthlyWorkingDays, int monthlyWorkingHours) {
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.monthlyWorkingDays = monthlyWorkingDays;
+        this.monthlyWorkingHours = monthlyWorkingHours;
+    }
+
+    public void employee() {
         /**
          * find the total wage and hours in month.
          */
-        System.out.println("Company name is "+companyName +" : ");
+        System.out.println("Company name is " + companyName + " : ");
         int empWage;
         int hour;
         int totalEmpWage = 0;
         int totalHours = 0;
         int days = 1;
-        while (days<=monthlyWorkingDays && totalHours<monthlyWorkingHours){
+        while (days <= monthlyWorkingDays && totalHours < monthlyWorkingHours) {
             /**
              * use the Math class and random(static method).
              */
-        int randomNumber = (int) (Math.random() * 10) % 3;
-        switch (randomNumber) {
-            case (FULL_DAY):
-                empWage = FULL_DAY_HOUR * wagePerHour;
-                hour=FULL_DAY_HOUR;
-                System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hour is "+hour);
-                break;
-            case (HALF_DAY):
-                empWage = HALF_DAY_HOUR * wagePerHour;
-                hour=HALF_DAY_HOUR;
-                System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hours is "+hour);
-                break;
-            default:
-                hour=0;
-                empWage=0;
-                System.out.println("Day - "+days+" : Employee is Absent");
+            int randomNumber = (int) (Math.random() * 10) % 3;
+            switch (randomNumber) {
+                case (FULL_DAY):
+                    empWage = FULL_DAY_HOUR * wagePerHour;
+                    hour = FULL_DAY_HOUR;
+                    System.out.println("Day - " + days + " : Employee wage is " + empWage + " and hour is " + hour);
+                    break;
+                case (HALF_DAY):
+                    empWage = HALF_DAY_HOUR * wagePerHour;
+                    hour = HALF_DAY_HOUR;
+                    System.out.println("Day - " + days + " : Employee wage is " + empWage + " and hours is " + hour);
+                    break;
+                default:
+                    hour = 0;
+                    empWage = 0;
+                    System.out.println("Day - " + days + " : Employee is Absent");
+            }
+            totalEmpWage = totalEmpWage + empWage;
+            totalHours = totalHours + hour;
+            days++;
         }
-        totalEmpWage=totalEmpWage+empWage;
-        totalHours=totalHours+hour;
-        days++;
-        }
-        System.out.println(companyName+" company total Employee wage : "+totalEmpWage);
-        System.out.println(companyName+" company total Employee working hours : "+totalHours);
+        System.out.println(companyName + " company total Employee wage : " + totalEmpWage);
+        System.out.println(companyName + " company total Employee working hours : " + totalHours);
     }
+
     public static void main(String[] args) {
         /**
-         * static method calling directly
+         * object level method calling through object reference name
          */
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter how many company details : ");
-        int numberOfCompanies = input.nextInt();
-        for (int i=1;i<=numberOfCompanies;i++){
-            System.out.print("Enter "+i+" company name : ");
-            String name = input.next();
-            System.out.print("Enter wage per hour : ");
-            int wagePerHour = input.nextInt();
-            System.out.print("Enter monthly working days : ");
-            int monthlyWorkingDays = input.nextInt();
-            System.out.print("Enter monthly working hour : ");
-            int monthlyWorkingHours = input.nextInt();
-            employee(name,wagePerHour,monthlyWorkingDays,monthlyWorkingHours);
-        }
+        EmployeeWage firstCompany = new EmployeeWage("Tata", 20, 25, 125);
+        EmployeeWage secondCompany = new EmployeeWage("jio", 18, 20, 100);
+        firstCompany.employee();
+        secondCompany.employee();
     }
+
 }
